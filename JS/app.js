7e2ad0bbf1328c7facba738007245fbcd40d4ee9
Shahjhansamoo <91805718+Shahjhansamoo@users.noreply.github.com>
2022-10-31 14:40:27 +0500

    initial commit

---
+++ JS/app.js
@@ -0,0 +1,102 @@
+alert("Thanks for your input!");
+
+
+
+// variable for string practice
+
+var name = "Mark";
+ var nationality = "U.S.";
+
+
+ alert(name + nationality );
+
+
+//  variable for string practice 2
+
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+
+
+
+alert(lastName + cityOfOrigin + aussieGreeting);
+
+
+// variable for number
+
+
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+
+
+alert(newNum);
+
+
+
+// variable for number 2
+
+
+var originalNumber = 23;
+var newNumber = originalNumber + 7;
+
+
+alert(newNumber);
+
+
+// some of the illegal variable are listed below which we can not use in code
+
+// alert
+// window
+// ActiveXObject
+// var
+// if
+// else
+
+
+
+// Math expressions
+
+
+var num = 10;
+var popularNumber = num + 200;
+
+// aleart(popularNumber);
+
+
+
+
+
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+
+console.log(popularNumber);
+
+
+
+
+
+// Math expressions Unfamilar
+
+// ++
+num++;
+
+ 
+num = num + 5;
+
+console.log(num)
+
+// --
+var newNum = --num;
+
+
+console.log(newNum + 100)
+
+
+// concatenating
+
+var userName = "shahjahan";
+var message = "Thanks, ";
+var banger = "!";
+alert(message + userName + banger);
